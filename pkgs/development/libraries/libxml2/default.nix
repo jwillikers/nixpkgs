96570aa30ff60bcba0153478bf416a8937590d71
Jordan Williams <jordan@jwillikers.com>
2024-07-27 13:02:06 -0500

    libxml2: Add version 2.13.3 and switch to Meson

--- pkgs/development/libraries/libxml2/default.nix
+++ pkgs/development/libraries/libxml2/default.nix
@@ -1,11 +1,14 @@
 { stdenv
 , lib
 , fetchurl
+, git
 , zlib
 , pkg-config
 , autoreconfHook
 , libintl
-, python
+, python3
+, meson
+, ninja
 , gettext
 , ncurses
 , findXMLCatalogs
@@ -17,44 +20,36 @@
 , icuSupport ? false
 , icu
 , enableShared ? !stdenv.hostPlatform.isMinGW && !stdenv.hostPlatform.isStatic
-, enableStatic ? !enableShared
 , gnome
 , testers
 }:
 
 stdenv.mkDerivation (finalAttrs: rec {
   pname = "libxml2";
-  version = "2.12.7";
+  version = "2.13.3";
 
-  outputs = [ "bin" "dev" "out" "doc" ]
-    ++ lib.optional pythonSupport "py"
-    ++ lib.optional (enableStatic && enableShared) "static";
+  outputs = [ "bin" "dev" "out" "doc" "man" ]
+    ++ lib.optional pythonSupport "py";
   outputMan = "bin";
 
   src = fetchurl {
     url = "mirror://gnome/sources/libxml2/${lib.versions.majorMinor version}/libxml2-${version}.tar.xz";
-    hash = "sha256-JK54/xNjqXPm2L66lBp5RdoqwFbhm1OVautpJ/1s+1Y=";
+    hash = "sha256-CAXXwYDPCcqtcWZsekWKdPBBVhpTKQJFTaUEfYOUgTg=";
   };
-
-  # https://gitlab.gnome.org/GNOME/libxml2/-/issues/725
-  postPatch = if stdenv.hostPlatform.isFreeBSD then ''
-    substituteInPlace ./configure.ac --replace-fail pthread_join pthread_create
-  '' else null;
 
   strictDeps = true;
 
   nativeBuildInputs = [
+    git
+    meson
+    ninja
     pkg-config
-    autoreconfHook
-  ];
+  ]
+  ++ lib.optionals pythonSupport [ python3 ];
 
-  buildInputs = lib.optionals pythonSupport [
-    python
-  ] ++ lib.optionals (pythonSupport && python?isPy2 && python.isPy2) [
+  buildInputs = [
     gettext
-  ] ++ lib.optionals (pythonSupport && python?isPy3 && python.isPy3) [
     ncurses
-  ] ++ lib.optionals (stdenv.isDarwin && pythonSupport && python?isPy2 && python.isPy2) [
     libintl
   ];
 
@@ -67,42 +62,23 @@ stdenv.mkDerivation (finalAttrs: rec {
     icu
   ];
 
-  configureFlags = [
-    "--exec-prefix=${placeholder "dev"}"
-    (lib.enableFeature enableStatic "static")
-    (lib.enableFeature enableShared "shared")
-    (lib.withFeature icuSupport "icu")
-    (lib.withFeature pythonSupport "python")
-    (lib.optionalString pythonSupport "PYTHON=${python.pythonOnBuildForHost.interpreter}")
-  ];
-
-  installFlags = lib.optionals pythonSupport [
-    "pythondir=\"${placeholder "py"}/${python.sitePackages}\""
-    "pyexecdir=\"${placeholder "py"}/${python.sitePackages}\""
-  ];
-
   enableParallelBuilding = true;
+
+  mesonFlags = [
+    "--python.platlibdir=${placeholder "py"}/${python3.sitePackages}"
+    "--python.purelibdir=${placeholder "py"}/${python3.sitePackages}"
+    "--mandir=${placeholder "man"}"
+    (lib.mesonEnable "icu" icuSupport)
+    (lib.mesonEnable "lzma" false)
+    (lib.mesonBool "python" pythonSupport)
+  ];
 
   doCheck =
     (stdenv.hostPlatform == stdenv.buildPlatform) &&
     stdenv.hostPlatform.libc != "musl";
-  preCheck = lib.optional stdenv.isDarwin ''
-    export DYLD_LIBRARY_PATH="$PWD/.libs:$DYLD_LIBRARY_PATH"
-  '';
 
   preConfigure = lib.optionalString (lib.versionAtLeast stdenv.hostPlatform.darwinMinVersion "11") ''
     MACOSX_DEPLOYMENT_TARGET=10.16
-  '';
-
-  preInstall = lib.optionalString pythonSupport ''
-    substituteInPlace python/libxml2mod.la --replace "$dev/${python.sitePackages}" "$py/${python.sitePackages}"
-  '';
-
-  postFixup = ''
-    moveToOutput bin/xml2-config "$dev"
-    moveToOutput lib/xml2Conf.sh "$dev"
-  '' + lib.optionalString (enableStatic && enableShared) ''
-    moveToOutput lib/libxml2.a "$static"
   '';
 
   passthru = {
