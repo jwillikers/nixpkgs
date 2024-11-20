6f143c81c8eb2c1b37f25b2bc50dc548fef879ea
Jordan Williams <jordan@jwillikers.com>
2024-11-20 09:35:44 -0600

    matio: avoid ac_cv_av_copy check to fix cross building
    Set ac_cv_av_copy to indicate the av_copy function exists.
    The function should exist in C99 implementations.
    
    Add passthru tests and update script.
    Remove use of pname.
    Reduce scope of lib.
    Add myself as maintainer.

--- pkgs/by-name/ma/matio/package.nix
+++ pkgs/by-name/ma/matio/package.nix
@@ -1,22 +1,51 @@
 {
-  lib,
-  stdenv,
   fetchurl,
+  hdf5,
+  lib,
+  matio,
+  nix-update-script,
+  pkgconf,
+  stdenv,
+  testers,
+  validatePkgConfig,
 }:
 stdenv.mkDerivation rec {
   pname = "matio";
   version = "1.5.27";
   src = fetchurl {
-    url = "mirror://sourceforge/matio/${pname}-${version}.tar.gz";
+    url = "mirror://sourceforge/matio/matio-${version}.tar.gz";
     sha256 = "sha256-CmqgCxjEUStjqNJ5BrB5yMbtQdSyhE96SuWY4Y0i07M=";
   };
 
-  meta = with lib; {
+  configureFlags = [ "ac_cv_va_copy=1" ];
+
+  nativeBuildInputs = [
+    pkgconf
+    validatePkgConfig
+  ];
+
+  buildInputs = [ hdf5 ];
+
+  passthru = {
+    tests = {
+      pkg-config = testers.hasPkgConfigModules {
+        package = matio;
+        versionCheck = true;
+      };
+      version = testers.testVersion {
+        package = matio;
+      };
+    };
+    updateScript = nix-update-script { };
+  };
+
+  meta = {
     description = "C library for reading and writing Matlab MAT files";
     homepage = "http://matio.sourceforge.net/";
-    license = licenses.bsd2;
-    maintainers = [ ];
+    license = with lib.licenses; [ bsd2 ];
+    maintainers = with lib.maintainers; [ jwillikers ];
     mainProgram = "matdump";
-    platforms = platforms.all;
+    platforms = lib.platforms.all;
+    pkgConfigModules = [ "matio" ];
   };
 }
